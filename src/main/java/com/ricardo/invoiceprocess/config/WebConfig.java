5758138e4a7f8bb46a1879648acd11bedccfb5d2
Ricardo Oliveira <ricardolima.ti@gmail.com>
2024-01-28 21:13:24 -0300

    Update WebConfig.java

--- src/main/java/com/ricardo/invoiceprocess/config/WebConfig.java
+++ src/main/java/com/ricardo/invoiceprocess/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.ricardo.invoiceprocess.service;
+package com.ricardo.invoiceprocess.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
